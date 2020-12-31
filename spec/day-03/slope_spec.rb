4a54895cb0cf4e4d67ec6b568952949522daf410
Mel Riffe <mel@juicyparts.com>
2020-12-30 22:48:28 -0500

    Move model-specific tests to a separate file

---
+++ spec/day-03/slope_spec.rb
@@ -0,0 +1,51 @@
+require './lib/day_03'
+
+RSpec.describe Slope do
+  let(:test_data) do
+    [
+      '..##.......',
+      '#...#...#..',
+      '.#....#..#.',
+      '..#.#...#.#',
+      '.#...##..#.',
+      '..#.##.....',
+      '.#.#.#....#',
+      '.#........#',
+      '#.##...#...',
+      '#...##....#',
+      '.#..#...#.#',
+    ]
+  end
+
+  context 'Samples' do
+    let(:model) { Day03.new test_data }
+
+    context 'with right 1 down 1 slope' do
+      it 'hits some trees' do
+        slope = Slope.new right: 1
+        expect(model.exercise1 slope).to eq 2
+      end
+    end
+
+    context 'with right 5 down 1 slope' do
+      it 'hits some trees' do
+        slope = Slope.new right: 5
+        expect(model.exercise1 slope).to eq 3
+      end
+    end
+
+    context 'with right 7 down 1 slope' do
+      it 'hits some trees' do
+        slope = Slope.new right: 7
+        expect(model.exercise1 slope).to eq 4
+      end
+    end
+
+    context 'with right 1 down 2 slope' do
+      it 'hits some trees' do
+        slope = Slope.new down: 2
+        expect(model.exercise1 slope).to eq 2
+      end
+    end
+  end
+end
--- spec/day-03/toboggan_trajectory_spec.rb
+++ spec/day-03/toboggan_trajectory_spec.rb
@@ -56,36 +56,4 @@ RSpec.describe 'Day 3: Toboggan Trajectory' do
       end
     end
   end
-
-  context 'Samples' do
-    let(:model) { Day03.new test_data }
-
-    context 'with right 1 down 1 slope' do
-      it 'hits some trees' do
-        slope = Slope.new right: 1
-        expect(model.exercise1 slope).to eq 2
-      end
-    end
-
-    context 'with right 5 down 1 slope' do
-      it 'hits some trees' do
-        slope = Slope.new right: 5
-        expect(model.exercise1 slope).to eq 3
-      end
-    end
-
-    context 'with right 7 down 1 slope' do
-      it 'hits some trees' do
-        slope = Slope.new right: 7
-        expect(model.exercise1 slope).to eq 4
-      end
-    end
-
-    context 'with right 1 down 2 slope' do
-      it 'hits some trees' do
-        slope = Slope.new down: 2
-        expect(model.exercise1 slope).to eq 2
-      end
-    end
-  end
 end
