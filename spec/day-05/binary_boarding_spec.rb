bdce0dd2315b9d4dfc90964e6d43650fbe2a68c5
Mel Riffe <mel@juicyparts.com>
2020-12-30 23:00:14 -0500

    Extract BoardingPass Spec into separate file

--- spec/day-05/binary_boarding_spec.rb
+++ spec/day-05/binary_boarding_spec.rb
@@ -37,24 +37,4 @@ RSpec.describe 'Day 5: Binary Boarding' do
       expect(model.exercise2).to eq 714
     end
   end
-
-  context 'with BoardingPass' do
-    let(:bording_pass) { BoardingPass.new code }
-
-    context 'when code is FBFBBFFRLR' do
-      let(:code) { 'FBFBBFFRLR' }
-
-      it 'returns 44 for row' do
-        expect(bording_pass.row).to eq 44
-      end
-
-      it 'returns 5 for column' do
-        expect(bording_pass.column).to eq 5
-      end
-
-      it 'returns 357 for seat id' do
-        expect(bording_pass.seat_id).to eq 357
-      end
-    end
-  end
 end
---
+++ spec/day-05/boarding_pass_spec.rb
@@ -0,0 +1,22 @@
+require './lib/day_05'
+
+RSpec.describe BoardingPass do
+
+  let(:bording_pass) { BoardingPass.new code }
+
+  context 'when code is FBFBBFFRLR' do
+    let(:code) { 'FBFBBFFRLR' }
+
+    it 'returns 44 for row' do
+      expect(bording_pass.row).to eq 44
+    end
+
+    it 'returns 5 for column' do
+      expect(bording_pass.column).to eq 5
+    end
+
+    it 'returns 357 for seat id' do
+      expect(bording_pass.seat_id).to eq 357
+    end
+  end
+end
