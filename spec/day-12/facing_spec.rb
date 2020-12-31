8a4ad07391935d2d25b854049fe0f33dad44675e
Mel Riffe <mel@juicyparts.com>
2020-12-30 23:09:45 -0500

    Extract model-specific tests into separate files

---
+++ spec/day-12/facing_spec.rb
@@ -0,0 +1,182 @@
+require './lib/day_12'
+
+RSpec.describe Facing do
+
+  subject { described_class.new test_key }
+
+  context 'with intial facing of :east' do
+    let(:test_key) { 0 }
+
+    it 'faces east' do
+      expect(subject.direction).to eq :east
+    end
+
+    context 'when turning left' do
+
+      it 'faces north with 1 turn' do
+        subject.turn_left 1
+        expect(subject.direction).to eq :north
+      end
+
+      it 'faces west with 2 turns' do
+        subject.turn_left 2
+        expect(subject.direction).to eq :west
+      end
+
+      it 'faces south with 3 turns' do
+        subject.turn_left 3
+        expect(subject.direction).to eq :south
+      end
+    end
+
+    context 'when turning right' do
+
+      it 'faces south with 1 turn' do
+        subject.turn_right 1
+        expect(subject.direction).to eq :south
+      end
+
+      it 'faces west with 2 turns' do
+        subject.turn_right 2
+        expect(subject.direction).to eq :west
+      end
+
+      it 'faces north with 3 turns' do
+        subject.turn_right 3
+        expect(subject.direction).to eq :north
+      end
+    end
+  end
+
+  context 'with intial facing of :south' do
+    let(:test_key) { 1 }
+
+    it 'faces south' do
+      expect(subject.direction).to eq :south
+    end
+
+    context 'when turning left' do
+
+      it 'faces east with 1 turn' do
+        subject.turn_left 1
+        expect(subject.direction).to eq :east
+      end
+
+      it 'faces north with 2 turns' do
+        subject.turn_left 2
+        expect(subject.direction).to eq :north
+      end
+
+      it 'faces west with 3 turns' do
+        subject.turn_left 3
+        expect(subject.direction).to eq :west
+      end
+    end
+
+    context 'when turning right' do
+
+      it 'faces west with 1 turn' do
+        subject.turn_right 1
+        expect(subject.direction).to eq :west
+      end
+
+      it 'faces north with 2 turns' do
+        subject.turn_right 2
+        expect(subject.direction).to eq :north
+      end
+
+      it 'faces east with 3 turns' do
+        subject.turn_right 3
+        expect(subject.direction).to eq :east
+      end
+    end
+  end
+
+  context 'with intial facing of :west' do
+    let(:test_key) { 2 }
+
+    it 'faces west' do
+      expect(subject.direction).to eq :west
+    end
+
+    context 'when turning left' do
+
+      it 'faces south with 1 turn' do
+        subject.turn_left 1
+        expect(subject.direction).to eq :south
+      end
+
+      it 'faces east with 2 turns' do
+        subject.turn_left 2
+        expect(subject.direction).to eq :east
+      end
+
+      it 'faces north with 3 turns' do
+        subject.turn_left 3
+        expect(subject.direction).to eq :north
+      end
+    end
+
+    context 'when turning right' do
+
+      it 'faces north with 1 turn' do
+        subject.turn_right 1
+        expect(subject.direction).to eq :north
+      end
+
+      it 'faces east with 2 turns' do
+        subject.turn_right 2
+        expect(subject.direction).to eq :east
+      end
+
+      it 'faces south with 3 turns' do
+        subject.turn_right 3
+        expect(subject.direction).to eq :south
+      end
+    end
+  end
+
+  context 'with intial facing of :north' do
+    let(:test_key) { 3 }
+
+    it 'faces north' do
+      expect(subject.direction).to eq :north
+    end
+
+    context 'when turning left' do
+
+      it 'faces west with 1 turn' do
+        subject.turn_left 1
+        expect(subject.direction).to eq :west
+      end
+
+      it 'faces south with 2 turns' do
+        subject.turn_left 2
+        expect(subject.direction).to eq :south
+      end
+
+      it 'faces east with 3 turns' do
+        subject.turn_left 3
+        expect(subject.direction).to eq :east
+      end
+    end
+
+    context 'when turning right' do
+
+      it 'faces east with 1 turn' do
+        subject.turn_right 1
+        expect(subject.direction).to eq :east
+      end
+
+      it 'faces south with 2 turns' do
+        subject.turn_right 2
+        expect(subject.direction).to eq :south
+      end
+
+      it 'faces west with 3 turns' do
+        subject.turn_right 3
+        expect(subject.direction).to eq :west
+      end
+    end
+  end
+end
---
+++ spec/day-12/location_spec.rb
@@ -0,0 +1,64 @@
+require './lib/day_12'
+
+RSpec.describe Location do
+
+  let(:test_facing) { Facing.new }
+  subject { described_class.new test_facing }
+
+  context '#manhattan_distance' do
+    before do
+      subject.east 17
+      subject.south 8
+    end
+
+    it 'calculates correct value' do
+      expect(subject.manhattan_distance).to eq 25
+    end
+  end
+
+  context 'when testing movement' do
+    context 'with north-south movement' do
+      it 'finishes with correct values' do
+        expect(subject.south_units).to be_zero
+        expect(subject.north_units).to be_zero
+
+        subject.south 5
+        expect(subject.south_units).to eq 5
+        expect(subject.north_units).to be_zero
+
+        subject.north 2
+        expect(subject.south_units).to eq 3
+        expect(subject.north_units).to be_zero
+
+        subject.north 3
+        expect(subject.south_units).to be_zero
+        expect(subject.north_units).to be_zero
+
+        subject.north 3
+        expect(subject.south_units).to be_zero
+        expect(subject.north_units).to eq 3
+
+        subject.south 11
+        expect(subject.south_units).to eq 8
+        expect(subject.north_units).to be_zero
+      end
+    end
+
+    context 'with east-west movement' do
+      before do
+        subject.west 2
+        subject.west 1
+        subject.west 4
+        subject.east 2
+      end
+
+      it 'has a zero east value' do
+        expect(subject.east_units).to be_zero
+      end
+
+      it 'has a five west value' do
+        expect(subject.west_units).to eq 5
+      end
+    end
+  end
+end
